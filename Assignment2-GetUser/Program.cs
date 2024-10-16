cca46fb3c06531b9502a4d15774bd035e1f1808a
TE-203 <TE-203@DESKTOP-ONS79RK>
2024-10-16 11:01:51 +0530

    Add project files.

---
+++ Assignment2-GetUser/Program.cs
@@ -0,0 +1,45 @@
+﻿
+using System;
+using System.IO;
+using OfficeOpenXml;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Set the license context for EPPlus.
+        ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+        List<string> userList = new List<string>();
+        string BackUppath = @"C:\Users\TE-203\Downloads\BackUpdata.xlsx";
+        using (var worksheetdata = new ExcelPackage(new FileInfo(BackUppath)))
+        {
+                var backUpdataworksheet = worksheetdata.Workbook.Worksheets[0]; // Access the first worksheet
+                var currentdataworksheet = worksheetdata.Workbook.Worksheets[1]; //Access the Second worksheet
+              
+                for (int i = 2; i <= backUpdataworksheet.Dimension.End.Row; i++)
+                {
+                    var BuserId = backUpdataworksheet.Cells[$"A{i}"].Text;
+                    for (int j = 2; j <= currentdataworksheet.Dimension.End.Row; j++)
+                    {
+                        var cuserId = currentdataworksheet.Cells[$"A{j}"].Text;
+                        if (BuserId == cuserId)
+                        {
+                            if (backUpdataworksheet.Cells[$"C{i}"].Text != currentdataworksheet.Cells[$"C{j}"].Text)
+                            { 
+                                userList.Add(backUpdataworksheet.Cells[$"D{i}"].Text);
+                            }
+                            break;
+                        }
+                    }
+                }    
+        }
+        int count = 1;
+        foreach (var item in userList)
+        {
+            Console.WriteLine((count++)+"->"+item);
+        }
+        Console.Read();
+    }
+}
+
+
